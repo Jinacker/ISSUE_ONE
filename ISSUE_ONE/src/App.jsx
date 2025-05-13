013e99ab1f190f9769bc4d5fbf7d3840ce528ece
Jinacker <rlawls1448@naver.com>
2025-05-13 23:03:00 +0900

    [FEAT] 페이지 이동 구현 (Link/Navigate Hook 사용) DATE:2025/05/13 1. 임시 페이지 이동 버튼 생성

--- ISSUE_ONE/src/App.jsx
+++ ISSUE_ONE/src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
-import {Routes, Route} from 'react-router-dom'
+
+import {Routes, Route, Link, useNavigate} from 'react-router-dom'
 
 import './App.css'
 import Home from './pages/Home'
@@ -11,13 +12,27 @@ import Notfound from './pages/Notfound'
 
 function App() {
 
+  const nav = useNavigate(); // navigation 함수 생성 => 원하는 페이지로 이동가능
+
+  const onClickButton = () => { // 이벤트 핸들러 생성 
+    nav("/search");
+  }
+
   return (
+    <>
+    <div>
+      <Link to = {"/"}>Home</Link>
+      <Link to = {"/load"}>Load</Link>
+      <Link to = {"/search"}>Search</Link>
+    </div>
+    <button onClick ={onClickButton}>Search 페이지로 이동</button>
     <Routes>
       <Route path = "/" element = {<Home></Home>}></Route>
       <Route path = "/load" element = {<Loading></Loading>}></Route>
       <Route path = "/search" element = {<Search></Search>}></Route>
       <Route path = "*" element = {<Notfound></Notfound>}></Route>
     </Routes>
+    </>
   )
 }
 
