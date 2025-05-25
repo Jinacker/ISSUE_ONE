52bfd89374c62e19fd86999596d090a8ee0e698b
Jinacker <rlawls1448@naver.com>
2025-05-25 14:35:58 +0900

    [FEAT] Build

--- ISSUE_ONE/src/pages/Notfound.jsx
+++ ISSUE_ONE/src/pages/Notfound.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Layout from '../layout/layout';
 
 // 검색 결과 페이지 
 
 const Notfound = () => {
     return(
+        <Layout>
         <div>404- 페이지를 찾을 수 없습니다.</div>
+        </Layout>
     )
 }
 
