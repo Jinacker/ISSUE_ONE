28d1976bc2f31a5c486a9efca197ac8d2373853e
Jinacker <rlawls1448@naver.com>
2025-05-25 14:35:00 +0900

    [FIX] App 컴포넌트 간소화

--- ISSUE_ONE/src/App.jsx
+++ ISSUE_ONE/src/App.jsx
@@ -1,15 +1,11 @@
 /* eslint-disable no-unused-vars */
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { ThemeProvider } from "styled-components";
-import GlobalStyle from "./GlobalStyle";
 
 import Home from "./pages/Home";
 import Loading from "./pages/Loading";
 import Search from "./pages/Search";
 import Notfound from "./pages/Notfound";
-import Header from "./layout/Header"; // ✅ Header 컴포넌트 분리된 경우
-import Layout from "./layout/layout";
 
 function App() {
   const nav = useNavigate();
