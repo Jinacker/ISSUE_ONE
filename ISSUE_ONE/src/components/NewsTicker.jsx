05b34da87de2f420113c2703c5993323ec53d375
Jinacker <rlawls1448@naver.com>
2025-06-01 03:34:47 +0900

    [FEAT] 신문사 로고 뻉뺑이 구현

---
+++ ISSUE_ONE/src/components/NewsTicker.jsx
@@ -0,0 +1,21 @@
+import './NewsTicker.css';
+import { newsLogos } from '../utils/newsLogos';
+
+const NewsTicker = () => {
+  const entries = Object.entries(newsLogos);
+  const fullLogos = [...entries, ...entries]; // 2배 복제
+
+  return (
+    <div className="news-ticker-wrapper">
+      <div className="news-ticker-track">
+        {fullLogos.map(([source, logo], idx) => (
+          <div className="logo-container" key={idx}>
+            <img src={logo} alt={source} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NewsTicker;
--- ISSUE_ONE/src/pages/Home.jsx
+++ ISSUE_ONE/src/pages/Home.jsx
@@ -3,12 +3,16 @@ import '../App.css';
 import Searchbar from '../components/Searchbar';
 import TrendingKeywords from '../components/TrendingKeywords';
 import Layout from '../layout/Layout';
+import NewsTicker from '../components/NewsTicker';
 
 const Home = () => {
 
   return (
       <Layout>
       <Searchbar></Searchbar>
+      <TrendingKeywords></TrendingKeywords>
+        <h2 style={{ marginLeft: '40px' }}>📰 함께하는 언론사</h2>
+        <NewsTicker />
       </Layout>
      );
 };
